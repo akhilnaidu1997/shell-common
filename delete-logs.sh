2e6252a6b850c7e7d29eee3e38ff2b450f567de2
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-29 17:49:33 +0530

    message

---
+++ delete-logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USER=$(id -u)
+
+LOG_FOLDER="/var/log/shell-practice"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+MONGOHOST="mongodb.daws86s-akhil.shop"
+SCRIPT_DIR=$PWD 
+START_TIME=$(date +%s)
+
+mkdir -p $LOG_FOLDER
+
+echo " Script started executing at : $(date)"
+
+SOURCE_DIR="/var/log/shell-practice"
+
+if [ ! -d $SOURCE_DIR]; then
+    echo "ERROR:: source directory not available"
+fi
+
+FIND_FILES=$(find . -name "*.log" -type f -mtime +14)
+
+while IFS=read -r line
+do
+    echo "Deleting lines: $line"
+done
